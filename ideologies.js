2308d99d37c2ca2e6466f75c5c6556e9bebb293f
Chris-dash-T4 <christofish2@gmail.com>
2021-05-05 18:05:28 -0400

    characters and calibration

--- ideologies.js
+++ ideologies.js
@@ -1,20 +1,21 @@
 ideologies = [
+    // Water
     {
-        "name": "Aang",
+        "name": "Katara",
         "stats": {
-            "comm": 50,
-            "strn": 29,
-            "sprt": 66,
-            "trad": 49
+            "comm": 61,
+            "strn": 28,
+            "sprt": 71,
+            "trad": 54
         }
     },
     {
-        "name": "Iroh",
+        "name": "Sokka",
         "stats": {
-            "comm": 45,
+            "comm": 58,
             "strn": 48,
-            "sprt": 66,
-            "trad": 63
+            "sprt": 51,
+            "trad": 46
         }
     },
     {
@@ -27,39 +28,22 @@ ideologies = [
         }
     },
     {
-        "name": "Toph",
-        "stats": {
-            "comm": 59,
-            "strn": 55,
-            "sprt": 43,
-            "trad": 45
-        }
-    },
-    {
-        "name": "Zaheer",
-        "stats": {
-            "comm": 28,
-            "strn": 39,
-            "sprt": 64,
-            "trad": 51
-        }
-    },
-    {
-        "name": "Ozai",
-        "stats": {
-            "comm": 53,
-            "strn": 79,
-            "sprt": 29,
-            "trad": 60
-        }
-    },
-    {
         "name": "Amon",
         "stats": {
             "comm": 64,
             "strn": 53,
             "sprt": 39,
             "trad": 34
+        }
+    },
+    // Earth
+    {
+        "name": "Toph Beifong",
+        "stats": {
+            "comm": 59,
+            "strn": 55,
+            "sprt": 43,
+            "trad": 45
         }
     },
     {
@@ -72,11 +56,76 @@ ideologies = [
         }
     },
     {
+        "name": "Kuvira",
+        "stats": {
+            "comm": 66,
+            "strn": 83,
+            "sprt": 24,
+            "trad": 52
+        }
+    },
+    // Fire
+    {
         "name": "Zuko",
         "stats": {
             "comm": 47,
             "strn": 69,
             "sprt": 32,
+            "trad": 51
+        }
+    },
+    {
+        "name": "Mai",
+        "stats": {
+            "comm": 48,
+            "strn": 69,
+            "sprt": 44,
+            "trad": 54
+        }
+    },
+    {
+        "name": "Ozai",
+        "stats": {
+            "comm": 42,
+            "strn": 85,
+            "sprt": 26,
+            "trad": 62
+        }
+    },
+    {
+        "name": "Iroh",
+        "stats": {
+            "comm": 51,
+            "strn": 57,
+            "sprt": 63,
+            "trad": 66
+        }
+    },
+    // Air
+    {
+        "name": "Aang",
+        "stats": {
+            "comm": 50,
+            "strn": 29,
+            "sprt": 66,
+            "trad": 49
+        }
+    },
+    {
+        "name": "Tenzin",
+        "stats": {
+            "comm": 52,
+            "strn": 36,
+            "sprt": 71,
+            "trad": 54
+        }
+    },
+    {
+        "name": "Zaheer",
+        "stats": {
+            "comm": 28,
+            "strn": 39,
+            "sprt": 64,
             "trad": 51
         }
         /*
